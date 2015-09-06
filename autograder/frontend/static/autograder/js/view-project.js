8972893f73b65b1ff4591eab9cc33ce8f6ea801c
James P <jameslp@umich.edu>
2015-09-05 22:15:23 -0400

    temp commit. finished refactoring project view.

--- autograder/frontend/static/autograder/js/view-project.js
+++ autograder/frontend/static/autograder/js/view-project.js
@@ -4,17 +4,11 @@ function load_project_submission_view(project_url)
 {
     console.log('load_project_view');
     var loaded = $.Deferred();
-    // console.log(project_url);
-    // var project_loaded = ;
     $.when(
         $.get(project_url)
     ).then(function(project) {
         return _get_or_register_group(project);
     }).then(function(group, project) {
-        console.log('wheee');
-        console.log(group);
-        console.log(project);
-
         $.when(
             lazy_get_template('project-submission-view'),
             lazy_get_template('submission-panel-list'),
@@ -31,7 +25,7 @@ function load_project_submission_view(project_url)
 
             $('#main-area').html(rendered);
 
-            _initialize_project_submission_view()
+            _initialize_project_submission_view(group, project);
 
             loaded.resolve();
         });
@@ -43,10 +37,9 @@ function load_project_submission_view(project_url)
 function _get_or_register_group(project)
 {
     console.log('_get_or_register_group');
-    console.log(project);
     var group_loaded = $.Deferred();
 
-    var url = get_submission_group_url(project.data.id, project.meta.username);
+    var url = _get_submission_group_url(project.data.id, project.meta.username);
     $.ajax(url,
     {
         statusCode: {
@@ -64,45 +57,38 @@ function _get_or_register_group(project)
         group_loaded.resolve(group, project);
     });
 
-    console.log('returning promise');
     return group_loaded.promise();
 }
 
 function _render_project_view(group, project, template, template_helpers)
 {
     console.log('_render_project_view');
-    console.log(group);
-    console.log(project);
-    console.log(template_helpers);
     var project_render_data = {
         'project': project,
         'group': group
     };
     var rendered = template.render(project_render_data, template_helpers);
-    console.log(rendered);
 
     return rendered;
 }
 
+// -----------------------------------------------------------------------------
+
 function _initialize_project_submission_view(group, project)
 {
     _initialize_submit_widget(group, project);
-    setup_collapsibles($('.submission-collapse'));
-    view_student_submissions_widget_init(project);
+    $('.submission-collapse').on('show.bs.collapse', _load_submission);
+    _initialize_view_student_submissions_widget(project);
 }
-
-// -----------------------------------------------------------------------------
 
 function _initialize_submit_widget(group, project)
 {
-    console.log('project rendered');
-    console.log($('#fileupload'));
     $('#fileupload').fileupload({
         'url': "/submissions/submission/",
         'dropZone': $('#dropzone'),
         'singleFileUploads': false,
         'done': function(event, response) {
-            on_submit_success(event, response, group.data.id);
+            _on_submit_success(event, response, group.data.id);
         }
     });
 
@@ -135,80 +121,34 @@ function _initialize_submit_widget(group, project)
     });
 }
 
-function on_submit_success(event, response, group_id)
-{
-    console.log(response);
-    var submission = response.result.data;
-    console.log(submission);
-    console.log('doneeee');
-
-    $('#upload-progress').empty();
-
-    // var node_id = "submission-" + String(submission.data.id) + "-" + String(group_id)
-
-    var submission_widget = $.parseHTML(
-        $.templates('#submission-collapse-panel').render(submission));
-
-    console.log(submission_widget);
-
-    var collapsible_context = $('<div>').append(submission_widget);
-    var collapsible = $('.submission-collapse', collapsible_context);
-    console.log(collapsible);
-    setup_collapsibles(collapsible);
-
-    $('#submission-list .panel').prepend(submission_widget);
-
-    collapsible.collapse();
-}
-
-function setup_collapsibles(selector)
-{
-    // console.log('setting up');
-    // console.log(selector);
-    selector.on('show.bs.collapse', function() {
-        // console.log('oiueqroiwuer');
-        var url = $('a', this).attr('href');
-        var context = $('.panel-body', this);
-        load_submission(url, context);
-    });
-}
-
-function load_submission(url, context)
-{
-    $.get(url).done(function(data, status) {
-        // console.log(data);
-        // console.log(context);
-        render_and_fix_links('view-submission', data, context);
-        var status = data.data.attributes.status;
-        if (status === 'being_graded' || status === 'received' ||
-            status === 'queued')
-        {
-            console.log('will try again a few seconds');
-            setTimeout(function() {load_submission(url, context);}, 5000);
-        }
-    });
-}
-
-function view_student_submissions_widget_init(project)
+function _initialize_view_student_submissions_widget(project)
 {
     $('#load-student-submissions-button').click(function(event){
         event.preventDefault();
         $('#student-submission-view .error').remove();
-        var email = $('#requested-email').val();
+        var email = $('#requested-email').val().trim();
         if (!is_umich_email(email))
         {
             $('#requested-email').after(
                 '<span class="error">Please enter a valid umich.edu email address</span>');
             return;
         }
-        var url = get_submission_group_url(
-            project.data.id, email);
-        $.get(
-            url
-        ).done(function (data, status) {
-            $.when(render_and_fix_links('submission-list', data, $('#student-submissions'))).done(
-                setup_collapsibles($('#student-submissions .submission-collapse'))
-            );
+        $.when(
+            $.get(_get_submission_group_url(project.data.id, email)),
+            lazy_get_template('submission-panel-list'),
+            lazy_get_template('submission-collapse-panel')
+        ).done(function(group_response, panel_list_tmpl, collapse_panel_tmpl) {
+            var template_context = {
+                submission_collapse_panel: collapse_panel_tmpl,
+                panel_id_suffix: 'student'
+            };
+            var rendered = panel_list_tmpl.render(
+                {group: group_response[0]}, template_context);
+            $('#student-submissions').html(rendered);
+            $('#student-submissions .collapse').on('show.bs.collapse', _load_submission);
+        }).fail(function() {
+            $('#requested-email').after(
+                '<span class="error">No submissions found for this student</span>');
         });
     });
     $('#requested-email').keypress(function(event) {
@@ -219,7 +159,64 @@ function view_student_submissions_widget_init(project)
     });
 }
 
-function get_submission_group_url(project_id, username)
+// -----------------------------------------------------------------------------
+
+function _on_submit_success(event, response, group_id)
+{
+    var submission = response.result.data;
+    $('#upload-progress').empty();
+
+    lazy_get_template('submission-collapse-panel').done(function(template) {
+        var rendered = $.parseHTML(template.render(submission));
+        $('#submission-list .panel').prepend(rendered);
+
+        var collapsible = $('#submission-' + String(submission.id));
+        collapsible.on('show.bs.collapse', _load_submission);
+        collapsible.collapse();
+    });
+}
+
+function _load_submission(event, url, render_location)
+{
+    if (url === undefined)
+    {
+        url = $('a', this).attr('href');
+    }
+    if (render_location === undefined)
+    {
+        render_location = $('.panel-body', this);
+    }
+
+    $.get(
+        url
+    ).then(function(submission) {
+        return _render_submission(submission, render_location)
+    }).then(function(submission) {
+        var status = submission.data.attributes.status;
+        if (status === 'being_graded' || status === 'received' ||
+            status === 'queued')
+        {
+            console.log('will try again a few seconds');
+            setTimeout(function() {
+                _load_submission(event, url, render_location);
+            }, 5000);
+        }
+    });
+}
+
+function _render_submission(submission, render_location)
+{
+    var finished_rendering = $.Deferred();
+
+    lazy_get_template('view-submission').done(function(template) {
+        render_location.html(template.render(submission));
+        finished_rendering.resolve(submission);
+    });
+
+    return finished_rendering.promise();
+}
+
+function _get_submission_group_url(project_id, username)
 {
     return "/submission-groups/submission-group/?" + $.param(
         {'project_id': project_id,
