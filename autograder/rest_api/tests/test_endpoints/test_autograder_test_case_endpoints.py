51c67bb4eac1f462b2e96ec633300b807621fff7
James P <jameslp@umich.edu>
2015-12-30 15:48:42 -0500

    wrote test cases for autograder test case endpoints.

--- autograder/rest_api/tests/test_endpoints/test_autograder_test_case_endpoints.py
+++ autograder/rest_api/tests/test_endpoints/test_autograder_test_case_endpoints.py
@@ -1,6 +1,7 @@
 import itertools
 
 from django.core.urlresolvers import reverse
+from django.core.exceptions import ObjectDoesNotExist
 
 from autograder.core.models import (
     AutograderTestCaseFactory, AutograderTestCaseBase)
@@ -37,32 +38,109 @@ class GetUpdateDeleteAutograderTestCaseTestCase(TemporaryFilesystemTestCase):
         self.project_url = reverse(
             'project:get', kwargs={'pk': self.project.pk})
 
-        # self.test_case =
+        self.test_case = AutograderTestCaseFactory.validate_and_create(
+            'compiled_and_run_test_case',
+            name='testy', project=self.project,
+            expected_return_code=0,
+            compiler='g++',
+            files_to_compile_together=self.required_filenames,
+            student_resource_files=self.required_filenames,
+            executable_name='prog')
+
+        self.test_url = reverse(
+            'ag-test:get', kwargs={'pk': self.test_case.pk})
 
     def test_course_admin_or_semester_staff_get_test_case(self):
-        self.fail()
+        for user in self.admin, self.staff:
+            client = MockClient(user)
+            response = client.get(self.test_url)
+
+            self.assertEqual(200, response.status_code)
+
+            expected_content = {
+                "urls": {
+                    "self": self.test_url,
+                    "project": self.project_url
+                }
+            }
+            expected_content.update(self.test_case.to_json())
+
+            self.assertEqual(
+                expected_content, json_load_bytes(response.content))
 
     def test_other_get_test_case_permission_denied(self):
-        self.fail()
+        for user in self.enrolled, self.nobody:
+            client = MockClient(user)
+            response = client.get(self.test_url)
 
-    def test_course_admin_get_test_case_not_found(self):
-        self.fail()
+            self.assertEqual(403, response.status_code)
 
     # -------------------------------------------------------------------------
 
     def test_course_admin_edit_test_case(self):
-        self.fail()
+        args = {
+            'files_to_compile_together': self.required_filenames[:1],
+            'name': 'westy',
+            'expected_standard_output': 'spamegg\n'
+        }
+
+        client = MockClient(self.admin)
+        response = client.patch(self.test_url, args)
+
+        self.assertEqual(200, response.status_code)
+
+        self.assertEqual(args, json_load_bytes(response.content))
+
+        loaded = AutograderTestCaseBase.objects.get(pk=self.test_case.pk)
+        for arg, value in args.items():
+            self.assertEqual(value, getattr(loaded, arg))
+
+        # sanity check that other fields weren't edited
+        self.assertEqual(
+            self.test_case.expected_return_code, loaded.expected_return_code)
 
     def test_course_admin_edit_test_case_invalid_settings(self):
-        self.fail()
+        args = {
+            'files_to_compile_together': ['not_a_student_file']
+        }
+
+        client = MockClient(self.admin)
+        response = client.patch(self.test_url, args)
+
+        self.assertEqual(400, response.status_code)
+
+        loaded = AutograderTestCaseBase.objects.get(pk=self.test_case.pk)
+
+        for arg in args:
+            self.assertEqual(
+                getattr(self.test_case, arg), getattr(loaded, arg))
 
     def test_other_edit_test_case_permission_denied(self):
-        self.fail()
+        for user in self.staff, self.enrolled, self.nobody:
+            client = MockClient(user)
+            response = client.patch(self.test_url, {'name': 'spam'})
+
+            self.assertEqual(403, response.status_code)
+
+            loaded = AutograderTestCaseBase.objects.get(pk=self.test_case.pk)
+            self.assertEqual(self.test_case.name, loaded.name)
 
     # -------------------------------------------------------------------------
 
     def test_course_admin_delete_test_case(self):
-        self.fail()
+        client = MockClient(self.admin)
+        response = client.delete(self.test_url)
+
+        self.assertEqual(204, response.status_code)
+
+        with self.assertRaises(ObjectDoesNotExist):
+            AutograderTestCaseBase.objects.get(pk=self.test_case.pk)
 
     def test_other_delete_test_case_permission_denied(self):
-        self.fail()
+        for user in self.staff, self.enrolled, self.nobody:
+            client = MockClient(user)
+            response = client.delete(self.test_url)
+
+            self.assertEqual(403, response.status_code)
+
+            AutograderTestCaseBase.objects.get(pk=self.test_case.pk)
