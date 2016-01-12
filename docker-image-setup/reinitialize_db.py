a1869adde8d0ddb275dc8f4da3dbd49220b0dfe5
James P <jameslp@umich.edu>
2016-01-11 23:50:34 -0500

    added docker container script that reinitializes a database

--- docker-image-setup/reinitialize_db.py
+++ docker-image-setup/reinitialize_db.py
@@ -2,18 +2,13 @@
 
 import argparse
 import MySQLdb
-import subprocess
 
-# This script does the following:
-#   1. Starts the appropriate database server
-#   2. Creates a database with the specified name
-#   3. Creates a user that has full permissions on the database
+# This script drops and re-creates the specified database
 
 
 def main():
     args = parse_args()
 
-    subprocess.call(['service', 'mysql', 'start'])
     _DB_HANDLERS[args.database_backend](args.database_name)
 
 
@@ -25,19 +20,21 @@ def parse_args():
     return parser.parse_args()
 
 
-def _init_mysql(db_name):
+def _reinitialize_mysql(db_name):
     server_connection = MySQLdb.connect()
     server_cursor = server_connection.cursor()
     # NOTE: The autograder database API ensures that db_name will only
     # contain alphabetic characters.
+    server_cursor.execute('DROP DATABASE {}'.format(db_name))
     server_cursor.execute('CREATE DATABASE {}'.format(db_name))
     server_cursor.execute('GRANT ALL ON {}.* to "autograder"'.format(db_name))
     server_cursor.close()
     server_connection.commit()
 
+
 # Map of string identifiers to handler functions
 _DB_HANDLERS = {
-    'mysql': _init_mysql
+    'mysql': _reinitialize_mysql
 }
 
 
