239b5f74dc750c7fae02745c5e10e358058cb18f
James P <jameslp@umich.edu>
2016-03-02 14:12:52 -0500

    implemented tests and functionality for sandbox __init__

--- autograder/security/autograder_sandbox.py
+++ autograder/security/autograder_sandbox.py
@@ -7,195 +7,317 @@ import autograder.core.shared.global_constants as gc
 
 
 SANDBOX_HOME_DIR_NAME = '/home/autograder'
-SANDBOX_WORKING_DIR_NAME = os.path.join(SANDBOX_HOME_DIR_NAME, 'working_dir')
 
 
-class AutograderSandbox(object):
-    def __init__(self, name, enable_networking=False,
-                 environment_variables_to_set=None,
-                 database_backend_to_use=None,
-                 database_name=''):
+class AutograderSandbox:
+    """
+    This class wraps Docker functionality to provide an interface for
+    running untrusted programs in a secure, isolated environment.
+
+    Docker documentation and installation instructions can be
+    found at: https://www.docker.com/
+
+    Instances of this class are intended to be used with a context manager.
+    """
+    def __init__(self, name=None, ip_address_whitelist=[],
+                 num_processes_soft_limit=None, num_processes_hard_limit=None,
+                 stack_size_soft_limit=None, stack_size_hard_limit=None,
+                 virtual_memory_soft_limit=None,
+                 virtual_memory_hard_limit=None,
+                 environment_variables=None):
+        """
+        Params:
+            name -- A human-readable name that can be used to identify
+                this sandbox instance. This value must be unique across
+                all sandbox instances, otherwise starting the sandbox
+                will fail.
+
+            ip_address_whitelist -- A list of IP addresses that programs
+                running inside this container should be allowed to access.
+                Access to all other IP addresses will be blocked.
+
+            ## The following fields set linux ulimit values inside the
+            ## sandbox. See the Linux ulimit documentation for more info.
+
+            num_processes_soft_limit -- The soft limit for ulimit -u
+
+            num_processes_hard_limit -- The hard limit for ulimit -u
+                If this value is not specified, then the soft limit
+                (if any) will be used.
+
+            stack_size_soft_limit -- The soft limit for ulimit -s
+                This value is in kilobytes.
+
+            stack_size_hard_limit -- The hard limit for ulimit -s
+                This value is in kilobytes.
+                If this value is not specified, then the soft limit
+                (if any) will be used.
+
+            virtual_memory_soft_limit -- The soft limit for ulimit -v
+                This value is in kilobytes.
+
+            virtual_memory_hard_limit -- The hard limit for ulimit -v
+                This value is in kilobytes.
+                If this value is not specified, then the soft limit
+                (if any) will be used.
+
+            environment_variables -- A dictionary of variable_name: value
+                pairs that should be set as environment variables inside
+                the sandbox.
+        """
         self._name = name
-        # self._linux_user_id = linux_user_id
-        # self._linux_username = 'worker{}'.format(self._linux_user_id)
-        self._linux_username = 'autograder'
-        self._networking_enabled = enable_networking
+        self._ip_address_whitelist = ip_address_whitelist
 
-        self._environment_variables = environment_variables_to_set
+        self._num_processes_soft_limit = num_processes_soft_limit
+        self._num_processes_hard_limit = num_processes_hard_limit
 
-        self._database_backend_to_use = database_backend_to_use
-        self._database_name = database_name
+        self._stack_size_soft_limit = stack_size_soft_limit
+        self._stack_size_hard_limit = stack_size_hard_limit
 
-        self._create_args = [
-            'docker', 'create', '--name=' + self.name,
-            '-a', 'STDOUT', '-a', 'STDERR', '-a', 'STDIN',  # Attach streams
-            '-i',  # Run in interactive mode (needed for input redirection)
-            '-t',  # Allocate psuedo tty
-            '--ulimit', 'nproc=4000',  # Limit number of user processes
-            # Allow or disallow networking
-            '--net', ('default' if enable_networking else 'none'),
-        ]
-        if self.environment_variables:
-            for key, value in self.environment_variables.items():
-                self._create_args += [
-                    '-e', "{}={}".format(key, value)
-                ]
+        self._virtual_memory_soft_limit = virtual_memory_soft_limit
+        self._virtual_memory_hard_limit = virtual_memory_hard_limit
 
-        self._create_args += [
-            'autograder',  # Image to use
-            'bash',  # Command to be run in the container
-        ]
-
-        subprocess.check_call(self.create_args, timeout=10)
+        self._environment_variables = environment_variables
 
     def __enter__(self):
-        self.start()
-        return self
+        pass
 
     def __exit__(self, *args):
-        self.stop()
-
-    def start(self):
-        print('starting container: ' + self.name)
-        try:
-            subprocess.check_call(['docker', 'start', self.name], timeout=10)
-        except subprocess.CalledProcessError:
-            self.stop()
-            raise
-
-        self.initialize_database()
-
-    def stop(self):
-        print('stopping container: ' + self.name)
-        subprocess.check_call(['docker', 'stop', self.name])
+        pass
 
     @property
     def name(self):
         return self._name
 
     @property
-    def networking_enabled(self):
-        return self._networking_enabled
+    def ip_address_whitelist(self):
+        return self._ip_address_whitelist
+
+    @property
+    def num_processes_soft_limit(self):
+        return self._num_processes_soft_limit
+
+    @property
+    def num_processes_hard_limit(self):
+        if self._num_processes_hard_limit is not None:
+            return self._num_processes_hard_limit
+
+        return self.num_processes_soft_limit
+
+    @property
+    def stack_size_soft_limit(self):
+        return self._stack_size_soft_limit
+
+    @property
+    def stack_size_hard_limit(self):
+        if self._stack_size_hard_limit is not None:
+            return self._stack_size_hard_limit
+
+        return self._stack_size_soft_limit
+
+    @property
+    def virtual_memory_soft_limit(self):
+        return self._virtual_memory_soft_limit
+
+    @property
+    def virtual_memory_hard_limit(self):
+        if self._virtual_memory_hard_limit is not None:
+            return self._virtual_memory_hard_limit
+
+        return self._virtual_memory_soft_limit
 
     @property
     def environment_variables(self):
         return self._environment_variables
 
-    @property
-    def database_backend_to_use(self):
-        return self._database_backend_to_use
 
-    @property
-    def database_name(self):
-        return self._database_name
 
-    @property
-    def create_args(self):
-        return self._create_args
+# class AutograderSandbox:
+#     def __init__(self, name, enable_networking=False,
+#                  environment_variables_to_set=None,
+#                  database_backend_to_use=None,
+#                  database_name=''):
+#         self._name = name
+#         # self._linux_user_id = linux_user_id
+#         # self._linux_username = 'worker{}'.format(self._linux_user_id)
+#         self._linux_username = 'autograder'
+#         self._networking_enabled = enable_networking
 
-    def copy_into_sandbox(self, *filenames):
-        """
-        Copies the specified files into the working directory of this
-        sandbox.
-        The filenames specified can be absolute paths or relative paths
-        to the current working directory.
-        """
-        for filename in filenames:
-            subprocess.check_call(
-                ['docker', 'cp', filename,
-                 self.name + ':' + SANDBOX_WORKING_DIR_NAME],
-                timeout=gc.DEFAULT_SUBPROCESS_TIMEOUT
-            )
+#         self._environment_variables = environment_variables_to_set
 
-        basenames = [os.path.basename(filename) for filename in filenames]
+#         self._database_backend_to_use = database_backend_to_use
+#         self._database_name = database_name
 
-        self.run_cmd_success_required(
-            ['chown', self._linux_username + ':' + self._linux_username] +
-            basenames, as_root=True)
+#         self._create_args = [
+#             'docker', 'create', '--name=' + self.name,
+#             '-a', 'STDOUT', '-a', 'STDERR', '-a', 'STDIN',  # Attach streams
+#             '-i',  # Run in interactive mode (needed for input redirection)
+#             '-t',  # Allocate psuedo tty
+#             '--ulimit', 'nproc=4000',  # Limit number of user processes
+#             # Allow or disallow networking
+#             '--net', ('default' if enable_networking else 'none'),
+#         ]
+#         if self.environment_variables:
+#             for key, value in self.environment_variables.items():
+#                 self._create_args += [
+#                     '-e', "{}={}".format(key, value)
+#                 ]
 
-    def copy_and_rename_into_sandbox(self, filename, new_name):
-        """
-        Copies the specified file into the working directory of this sandbox
-        and renames the copy to new_name.
-        new_name must consist of only a filename. Any other path information
-        will be stripped.
-        The filename specified can be an absolute path or a relative path
-        to the current working directory.
-        """
-        copy_destination = self.name + ':' + os.path.join(
-            SANDBOX_WORKING_DIR_NAME, os.path.basename(new_name))
+#         self._create_args += [
+#             'autograder',  # Image to use
+#             'bash',  # Command to be run in the container
+#         ]
 
-        print(copy_destination)
+#         subprocess.check_call(self.create_args, timeout=10)
 
-        subprocess.check_call(
-            ['docker', 'cp', filename, copy_destination],
-            timeout=gc.DEFAULT_SUBPROCESS_TIMEOUT)
+#     def __enter__(self):
+#         self.start()
+#         return self
 
-        self.run_cmd_success_required(
-            ['chown', self._linux_username + ':' + self._linux_username,
-             new_name], as_root=True)
+#     def __exit__(self, *args):
+#         self.stop()
 
-    def run_cmd_with_redirected_io(self, cmd_exec_args,
-                                   as_root=False,
-                                   timeout=gc.DEFAULT_SUBPROCESS_TIMEOUT,
-                                   stdin_content=''):
-        args = ['docker', 'exec', '-i']
-        if not as_root:
-            args.append('--user={}'.format(self._linux_username))
-        args.append(self.name)
-        args += ['timeout_script.py', str(timeout)] + cmd_exec_args
+#     def start(self):
+#         print('starting container: ' + self.name)
+#         try:
+#             subprocess.check_call(['docker', 'start', self.name], timeout=10)
+#         except subprocess.CalledProcessError:
+#             self.stop()
+#             raise
 
-        print('running: {}'.format(args))
+#         self.initialize_database()
 
-        runner = _SubprocessRunner(
-            args, timeout=timeout * 2, stdin_content=stdin_content)
-        return runner
+#     def stop(self):
+#         print('stopping container: ' + self.name)
+#         subprocess.check_call(['docker', 'stop', self.name])
 
-    def run_cmd_success_required(self, cmd_exec_args, as_root=False,
-                                 timeout=gc.DEFAULT_SUBPROCESS_TIMEOUT,
-                                 return_output=True):
-        args = ['docker', 'exec']
-        if not as_root:
-            args.append('--user={}'.format(self._linux_username))
-        args.append(self.name)
-        args += cmd_exec_args
+#     @property
+#     def name(self):
+#         return self._name
 
-        if return_output:
-            return subprocess.check_output(
-                args, timeout=timeout, universal_newlines=True)
+#     @property
+#     def networking_enabled(self):
+#         return self._networking_enabled
 
-        subprocess.check_call(args, timeout=timeout)
+#     @property
+#     def environment_variables(self):
+#         return self._environment_variables
 
-    def clear_working_dir(self):
-        working_dir_contents = self.run_cmd_success_required(
-            ['ls', SANDBOX_WORKING_DIR_NAME]
-        ).split()
-        working_dir_contents = [
-            os.path.join(SANDBOX_WORKING_DIR_NAME, filename)
-            for filename in working_dir_contents]
+#     @property
+#     def database_backend_to_use(self):
+#         return self._database_backend_to_use
 
-        backup_dirname = os.path.join(
-            SANDBOX_HOME_DIR_NAME, "old-" + uuid.uuid4().hex)
-        self.run_cmd_success_required(["mkdir", "-p", backup_dirname])
+#     @property
+#     def database_name(self):
+#         return self._database_name
 
-        self.run_cmd_success_required(
-            ["mv"] + working_dir_contents + [backup_dirname])
+#     @property
+#     def create_args(self):
+#         return self._create_args
 
-    def initialize_database(self):
-        if not self.database_backend_to_use:
-            return
+#     def copy_into_sandbox(self, *filenames):
+#         """
+#         Copies the specified files into the working directory of this
+#         sandbox.
+#         The filenames specified can be absolute paths or relative paths
+#         to the current working directory.
+#         """
+#         for filename in filenames:
+#             subprocess.check_call(
+#                 ['docker', 'cp', filename,
+#                  self.name + ':' + SANDBOX_WORKING_DIR_NAME],
+#                 timeout=gc.DEFAULT_SUBPROCESS_TIMEOUT
+#             )
 
-        self.run_cmd_success_required(
-            ['initialize_db', self.database_backend_to_use,
-             self.database_name], as_root=True)
+#         basenames = [os.path.basename(filename) for filename in filenames]
 
-    def reinitialize_database(self):
-        if not self.database_backend_to_use:
-            return
+#         self.run_cmd_success_required(
+#             ['chown', self._linux_username + ':' + self._linux_username] +
+#             basenames, as_root=True)
 
-        self.run_cmd_success_required(
-            ['reinitialize_db', self.database_backend_to_use,
-             self.database_name], as_root=True)
+#     def copy_and_rename_into_sandbox(self, filename, new_name):
+#         """
+#         Copies the specified file into the working directory of this sandbox
+#         and renames the copy to new_name.
+#         new_name must consist of only a filename. Any other path information
+#         will be stripped.
+#         The filename specified can be an absolute path or a relative path
+#         to the current working directory.
+#         """
+#         copy_destination = self.name + ':' + os.path.join(
+#             SANDBOX_WORKING_DIR_NAME, os.path.basename(new_name))
+
+#         print(copy_destination)
+
+#         subprocess.check_call(
+#             ['docker', 'cp', filename, copy_destination],
+#             timeout=gc.DEFAULT_SUBPROCESS_TIMEOUT)
+
+#         self.run_cmd_success_required(
+#             ['chown', self._linux_username + ':' + self._linux_username,
+#              new_name], as_root=True)
+
+#     def run_cmd_with_redirected_io(self, cmd_exec_args,
+#                                    as_root=False,
+#                                    timeout=gc.DEFAULT_SUBPROCESS_TIMEOUT,
+#                                    stdin_content=''):
+#         args = ['docker', 'exec', '-i']
+#         if not as_root:
+#             args.append('--user={}'.format(self._linux_username))
+#         args.append(self.name)
+#         args += ['timeout_script.py', str(timeout)] + cmd_exec_args
+
+#         print('running: {}'.format(args))
+
+#         runner = _SubprocessRunner(
+#             args, timeout=timeout * 2, stdin_content=stdin_content)
+#         return runner
+
+#     def run_cmd_success_required(self, cmd_exec_args, as_root=False,
+#                                  timeout=gc.DEFAULT_SUBPROCESS_TIMEOUT,
+#                                  return_output=True):
+#         args = ['docker', 'exec']
+#         if not as_root:
+#             args.append('--user={}'.format(self._linux_username))
+#         args.append(self.name)
+#         args += cmd_exec_args
+
+#         if return_output:
+#             return subprocess.check_output(
+#                 args, timeout=timeout, universal_newlines=True)
+
+#         subprocess.check_call(args, timeout=timeout)
+
+#     def clear_working_dir(self):
+#         working_dir_contents = self.run_cmd_success_required(
+#             ['ls', SANDBOX_WORKING_DIR_NAME]
+#         ).split()
+#         working_dir_contents = [
+#             os.path.join(SANDBOX_WORKING_DIR_NAME, filename)
+#             for filename in working_dir_contents]
+
+#         backup_dirname = os.path.join(
+#             SANDBOX_HOME_DIR_NAME, "old-" + uuid.uuid4().hex)
+#         self.run_cmd_success_required(["mkdir", "-p", backup_dirname])
+
+#         self.run_cmd_success_required(
+#             ["mv"] + working_dir_contents + [backup_dirname])
+
+#     def initialize_database(self):
+#         if not self.database_backend_to_use:
+#             return
+
+#         self.run_cmd_success_required(
+#             ['initialize_db', self.database_backend_to_use,
+#              self.database_name], as_root=True)
+
+#     def reinitialize_database(self):
+#         if not self.database_backend_to_use:
+#             return
+
+#         self.run_cmd_success_required(
+#             ['reinitialize_db', self.database_backend_to_use,
+#              self.database_name], as_root=True)
 
 
 # TODO: Once upgraded to Python 3.5, replace call() with the
--- autograder/security/tests.py
+++ autograder/security/tests.py
@@ -1,20 +1,172 @@
-from django.test import TestCase
+import unittest
+import subprocess
+
+from .autograder_sandbox import AutograderSandbox
 
 
-class AutograderSandboxTestCase(TestCase):
-    import unittest
-    @unittest.skip('todo')
-    def test_sandbox_default_init(self):
+# class AutograderSandboxTestCase(unittest.TestCase):
+
+class AutograderSandboxInitTestCase(unittest.TestCase):
+    def setUp(self):
+        self.name = 'awexome_container'
+        self.ip_whitelist = ['35.2.65.126']
+        self.num_processes_soft = 4
+        self.num_processes_hard = 6
+        self.stack_size_soft = 20000
+        self.stack_size_hard = 40000
+        self.virtual_memory_soft = 500000
+        self.virtual_memory_hard = 1000000
+        self.environment_variables = {'spam': 'egg', 'sausage': 42}
+
+    def test_default_init(self):
+        sandbox = AutograderSandbox()
+        self.assertIsNone(sandbox.name)
+        self.assertCountEqual([], sandbox.ip_address_whitelist)
+        self.assertIsNone(sandbox.num_processes_soft_limit)
+        self.assertIsNone(sandbox.num_processes_hard_limit)
+        self.assertIsNone(sandbox.stack_size_soft_limit)
+        self.assertIsNone(sandbox.stack_size_hard_limit)
+        self.assertIsNone(sandbox.virtual_memory_soft_limit)
+        self.assertIsNone(sandbox.virtual_memory_hard_limit)
+        self.assertIsNone(sandbox.environment_variables)
+
+    def test_non_default_init(self):
+        sandbox = AutograderSandbox(
+            name=self.name,
+            ip_address_whitelist=self.ip_whitelist,
+            num_processes_soft_limit=self.num_processes_soft,
+            num_processes_hard_limit=self.num_processes_hard,
+            stack_size_soft_limit=self.stack_size_soft,
+            stack_size_hard_limit=self.stack_size_hard,
+            virtual_memory_soft_limit=self.virtual_memory_soft,
+            virtual_memory_hard_limit=self.virtual_memory_hard,
+            environment_variables=self.environment_variables
+        )
+
+        self.assertEqual(self.name,
+                         sandbox.name)
+        self.assertEqual(self.ip_whitelist,
+                         sandbox.ip_address_whitelist)
+        self.assertEqual(self.num_processes_soft,
+                         sandbox.num_processes_soft_limit)
+        self.assertEqual(self.num_processes_hard,
+                         sandbox.num_processes_hard_limit)
+        self.assertEqual(self.stack_size_soft,
+                         sandbox.stack_size_soft_limit)
+        self.assertEqual(self.stack_size_hard,
+                         sandbox.stack_size_hard_limit)
+        self.assertEqual(self.virtual_memory_soft,
+                         sandbox.virtual_memory_soft_limit)
+        self.assertEqual(self.virtual_memory_hard,
+                         sandbox.virtual_memory_hard_limit)
+        self.assertEqual(self.environment_variables,
+                         sandbox.environment_variables)
+
+    def test_init_soft_limits_only(self):
+        """
+        Makes sure that when only soft limits are specified, the hard limits
+        are given the same values.
+        """
+        sandbox = AutograderSandbox(
+            num_processes_soft_limit=self.num_processes_soft,
+            stack_size_soft_limit=self.stack_size_soft,
+            virtual_memory_soft_limit=self.virtual_memory_soft)
+
+        self.assertEqual(self.num_processes_soft,
+                         sandbox.num_processes_soft_limit)
+        self.assertEqual(self.num_processes_hard,
+                         sandbox.num_processes_hard_limit)
+        self.assertEqual(self.stack_size_soft,
+                         sandbox.stack_size_soft_limit)
+        self.assertEqual(self.stack_size_hard,
+                         sandbox.stack_size_hard_limit)
+        self.assertEqual(self.virtual_memory_soft,
+                         sandbox.virtual_memory_soft_limit)
+        self.assertEqual(self.virtual_memory_hard,
+                         sandbox.virtual_memory_hard_limit)
+
+
+class AutograderSandboxBasicRunCommandTestCase(unittest.TestCase):
+    def setUp(self):
+        self.sandbox = AutograderSandbox()
+
+        self.root_cmd = ["touch", "/"]
+
+    def test_run_legal_command_non_root(self):
+        stdout_content = "hello world"
+        with self.sandbox:
+            cmd_result = self.sandbox.run_command(["echo", stdout_content])
+            self.assertEqual(0, cmd_result.return_code)
+            self.assertEqual(stdout_content, cmd_result.stdout)
+
+    def test_run_illegal_command_non_root(self):
+        with self.sandbox:
+            cmd_result = self.sandbox.run_command(self.root_cmd)
+            self.assertNotEqual(0, cmd_result.return_code)
+            self.assertNotEqual("", cmd_result.stderr)
+
+    def test_run_command_as_root(self):
+        with self.sandbox:
+            cmd_result = self.sandbox.run_command(self.root_cmd)
+            self.assertEqual(0, cmd_result.return_code)
+            self.assertEqual("", cmd_result.stderr)
+
+    def test_run_command_timeout_exceeded(self):
         self.fail()
 
-    @unittest.skip('todo')
-    def test_sandbox_networking_enabled(self):
+    def test_run_command_raise_on_error(self):
+        """
+        Tests that an exception is thrown only when raise_on_failure is True
+        and the command exits with nonzero status.
+        """
+        with self.sandbox:
+            # No exception should be raised.
+            cmd_result = self.sandbox.run_command(self.root_cmd,
+                                                  as_root=True,
+                                                  raise_on_failure=True)
+            self.assertEqual(0, cmd_result.return_code)
+
+            with self.assertRaises(subprocess.CalledProcessError):
+                self.sandbox.run_command(self.root_cmd, raise_on_failure=True)
+
+
+class AutograderSandboxUlimitTestCase(unittest.TestCase):
+    def test_non_root_command_exceeds_ulimit(self):
         self.fail()
 
-    @unittest.skip('todo')
-    def test_sandbox_networking_disabled(self):
+    def test_multiple_containers_dont_exceed_ulimits(self):
+        """
+        One quirk of docker containers is that if there are multiple users
+        created in different containers but with the same UID, the resource
+        usage of all those users will contribute to hitting the same ulimits.
+        This test makes sure that users are created with different UIDs
+        and don't step on each others' resource limits.
+        """
         self.fail()
 
-    @unittest.skip('todo')
+
+class AutograderSandboxNetworkAccessTestCase(unittest.TestCase):
+    def test_networking_disabled(self):
+        self.fail()
+
+    def test_run_command_access_ip_address_whitelist(self):
+        self.fail()
+
+
+class AutograderSandboxCopyFilesTestCase(unittest.TestCase):
+    def test_copy_files_into_sandbox(self):
+        self.fail()
+
+    def test_copy_and_rename_file_into_sandbox(self):
+        self.fail()
+
+
+class AutograderSandboxMiscTestCase(unittest.TestCase):
+    def test_reset(self):
+        self.fail()
+
+    def test_context_manager(self):
+        self.fail()
+
     def test_sandbox_environment_variables_set(self):
         self.fail()
