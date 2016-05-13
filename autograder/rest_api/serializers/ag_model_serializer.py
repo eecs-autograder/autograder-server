6e8a861e5491a68961c7668fbebe39c5f2802247
James P <jameslp@umich.edu>
2016-05-12 20:44:25 -0400

    implemented autograder model serializer base class.

--- autograder/rest_api/serializers/ag_model_serializer.py
+++ autograder/rest_api/serializers/ag_model_serializer.py
@@ -1,9 +1,32 @@
 from rest_framework import serializers
 
 
-class AGModelSerializer(serializers.Serializer):
-    def to_representation(self, obj):
-        pass
+class AGModelSerializer(serializers.BaseSerializer):
+    def get_ag_model_manager(self):
+        """
+        Returns a django model manager object that can be used to create
+        objects of the desired autograder model type.
+        """
+        raise NotImplementedError(
+            "Derived classes must override this method")
 
+    def to_representation(self, obj):
+        return obj.to_dict()
+
+    # Subclasses may need to override this if any sub-objects need to be
+    # deserialized (for example, FeedbackConfig objects)
     def to_internal_value(self, data):
-        pass
+        return data
+
+    def create(self, validated_data):
+        return self.get_ag_model_manager().validate_and_create(
+            **validated_data)
+
+    def update(self, instance, validated_data):
+        instance.validate_and_update(**validated_data)
+        return instance
+
+    # Since we're pushing the validation down to the database
+    # level, this method should do nothing.
+    def run_validation(self, initial_data):
+        return initial_data
--- autograder/rest_api/tests/test_serializers/test_ag_model_serializer.py
+++ autograder/rest_api/tests/test_serializers/test_ag_model_serializer.py
@@ -1,16 +1,80 @@
+import random
+
 from django import test
-from autograder.core.tests.test_models import models
+from django.core import exceptions
+# from rest_framework import parsers
+
+from autograder.core.tests.test_models.models import _DummyAutograderModel
+
+from autograder.rest_api.serializers.ag_model_serializer import (
+    AGModelSerializer)
+
+
+class _DummyAutograderModelSerialier(AGModelSerializer):
+    def get_ag_model_manager(self):
+        return _DummyAutograderModel.objects
 
 
 class AGModelSerializerTestCase(test.TestCase):
-    def test_to_representation(self):
-        self.fail()
+    def setUp(self):
+        self.ag_model = _DummyAutograderModel(
+            pos_num_val=42,
+            non_empty_str_val="spam")
 
-    def test_to_internal_value(self):
-        self.fail()
+    def test_serialize(self):
+        serializer = _DummyAutograderModelSerialier(self.ag_model)
+        self.assertEqual(self.ag_model.to_dict(), serializer.data)
 
     def test_create(self):
-        self.fail()
+        self.assertEqual(0, _DummyAutograderModel.objects.count())
+        serializer = _DummyAutograderModelSerialier(
+            data=self.ag_model.to_dict())
+
+        self.assertTrue(serializer.is_valid())
+        serializer.save()
+
+        self.assertEqual(1, _DummyAutograderModel.objects.count())
+        loaded = _DummyAutograderModel.objects.get(
+            pos_num_val=self.ag_model.pos_num_val)
+        self.assertEqual(self.ag_model.non_empty_str_val,
+                         loaded.non_empty_str_val)
+
+    def test_create_with_field_errors(self):
+        with self.assertRaises(exceptions.ValidationError):
+            serializer = _DummyAutograderModelSerialier(
+                data={'pos_num_val': -2, 'non_empty_str_val': ''})
+
+            self.assertTrue(serializer.is_valid())
+            serializer.save()
+
+        self.assertEqual(0, _DummyAutograderModel.objects.count())
 
     def test_update(self):
-        self.fail()
+        self.ag_model.save()
+        new_pos_num_val = random.randint(1000, 9000)
+        serializer = _DummyAutograderModelSerialier(
+            self.ag_model,
+            data={'pos_num_val': new_pos_num_val},
+            partial=True)
+
+        self.assertTrue(serializer.is_valid())
+        serializer.save()
+
+        self.ag_model.refresh_from_db()
+
+        self.assertEqual(new_pos_num_val, self.ag_model.pos_num_val)
+
+    def test_update_with_field_errors(self):
+        self.ag_model.save()
+        bad_pos_num_val = -8
+        with self.assertRaises(exceptions.ValidationError):
+            serializer = _DummyAutograderModelSerialier(
+                self.ag_model,
+                data={'pos_num_val': bad_pos_num_val},
+                partial=True)
+
+            self.assertTrue(serializer.is_valid())
+            serializer.save()
+
+        self.ag_model.refresh_from_db()
+        self.assertNotEqual(bad_pos_num_val, self.ag_model.pos_num_val)
