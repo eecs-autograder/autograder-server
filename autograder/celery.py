12e4e2165c7c8956264e7b2a05c376ec3781006e
James Louis Perretta <jameslp@class9.eecs.umich.edu>
2016-09-14 16:41:07 -0400

    updated some settings

--- autograder/celery.py
+++ autograder/celery.py
@@ -14,20 +14,24 @@ app = Celery('autograder', backend='amqp')
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
 if not os.environ.get('IS_CELERYBEAT', False):
+    print('autodiscovering tasks')
     app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 @app.task
 def queue_submissions():
     import autograder.core.models as ag_models
+    from autograder.grading_tasks.tasks import grade_submission
     # TODO: integration test
     # TODO: update this to support multiple courses in one system
     with transaction.atomic():
-        newly_queued = ag_models.Submission.objects.select_for_update().filter(
-            status=ag_models.Submission.GradingStatus.received)
-        newly_queued.update(status=ag_models.Submission.GradingStatus.queued)
+        to_queue = list(ag_models.Submission.objects.select_for_update().filter(
+            status=ag_models.Submission.GradingStatus.received))
+        print(to_queue)
 
-        print(newly_queued.all())
-        for submission in newly_queued.all():
-            grade_submission.apply_async(submission.pk)
+        for submission in to_queue: 
+            print('adding submission{} to queue for grading'.format(submission.pk))
+            submission.status = 'queued'
+            submission.save()
+            grade_submission.apply_async([submission.pk])
 
-        print('queued {} submissions'.format(len(newly_queued)))
+        print('queued {} submissions'.format(to_queue))
--- autograder/settings/celery_config.py
+++ autograder/settings/celery_config.py
@@ -11,18 +11,18 @@ CELERYD_CONCURRENCY = 1
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_ACCEPT_CONTENT = ['json']  # Ignore other content
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_QUEUES = (
-    Queue('submissions'),
-    Queue('deferred'),
-    Queue('submission_listener')
-)
+#CELERY_QUEUES = (
+#    Queue('submissions'),
+#    Queue('deferred'),
+#    Queue('submission_listener')
+#)
 
-CELERYBEAT_SCHEDULE = {
-    'queue-submissions': {
-        'task': 'autograder.celery.queue_submissions',
-        'schedule': datetime.timedelta(seconds=5),  # UPDATE AS DESIRED
-        'options': {
-            'queue': 'submission_listener'
-        }
-    },
-}
+#CELERYBEAT_SCHEDULE = {
+#    'queue-submissions': {
+#        'task': 'autograder.celery.queue_submissions',
+#        'schedule': datetime.timedelta(seconds=5),  # UPDATE AS DESIRED
+#        'options': {
+#            'queue': 'submission_listener'
+#        }
+#    },
+#}
