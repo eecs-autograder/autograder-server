8361de0f05c239d157296241a57bf54f83b88562
James P <jameslp@umich.edu>
2016-10-04 20:05:55 -0400

    updated manage.py so that when we're running tests, we use the dev settings by default

--- manage.py
+++ manage.py
@@ -3,7 +3,11 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "autograder.settings")
+    if sys.argv[1] == 'test':
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "autograder.settings.development")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "autograder.settings")
+
     os.environ.setdefault("DJANGO_TEST_PROCESSES", '1')
 
     from django.core.management import execute_from_command_line
