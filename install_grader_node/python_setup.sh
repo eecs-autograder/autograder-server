af3ae70db095ea1356e845f1f77da0d4b48acec5
James P <jameslp@umich.edu>
2017-01-20 21:04:50 -0500

    Working on condensing ssh tunnel systemd scripts

--- install_grader_node/python_setup.sh
+++ install_grader_node/python_setup.sh
@@ -4,11 +4,8 @@
 export DJANGO_SETTINGS_MODULE="autograder.settings.production"
 
 sudo apt-get update
-sudo apt-get install -y \
-    python3-wheel python3-pip python3-venv libncurses5-dev libxml2-dev libxslt1-dev
+sudo apt-get install -y python3-wheel python3-pip python3-venv
 sudo pip3 install --upgrade pip
 
-sudo pip3 install --upgrade pip
 cd .. && sudo pip3 install -r requirements.txt && cd -
-cd ../autograder-sandbox && sudo python3 setup.py install && cd -
 
