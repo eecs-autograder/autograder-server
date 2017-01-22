4558f8b7bc8dc76441190086edd4ec4752d194db
James P <jameslp@umich.edu>
2017-01-22 17:51:53 -0600

    added postgres install commands to python_setup grader node script

--- install_grader_node/python_setup.sh
+++ install_grader_node/python_setup.sh
@@ -3,8 +3,9 @@
 
 export DJANGO_SETTINGS_MODULE="autograder.settings.production"
 
-sudo apt-get update
-sudo apt-get install -y python3-wheel python3-pip python3-venv
+sudo apt update
+sudo apt install -y postgresql-9.5 postgresql-server-dev-9.5
+sudo apt install -y python3-wheel python3-pip python3-venv
 sudo pip3 install --upgrade pip
 
 cd .. && sudo pip3 install -r requirements.txt && cd -
