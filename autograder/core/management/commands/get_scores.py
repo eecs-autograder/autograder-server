75f7bb1f18ac9400d0eec5136856a382d069ddc1
James P <jameslp@umich.edu>
2017-04-27 14:15:18 -0400

    Updated get scores output format so that usernames are in separate columns and test case names are sorted.

--- autograder/core/management/commands/get_scores.py
+++ autograder/core/management/commands/get_scores.py
@@ -1,33 +1,35 @@
 #! /usr/bin/env python3
 
 import csv
+import itertools
 # import traceback
 
 from django.core.management.base import BaseCommand
 
-from autograder.core.models import Course
+from autograder.core.models import Project
 
 
 class Command(BaseCommand):
     help = 'Dumps ultimate submission score breakdown to a csv file'
 
     def add_arguments(self, parser):
-        parser.add_argument('course_name')
-        parser.add_argument('project_name')
+        parser.add_argument('project_pk')
         parser.add_argument('csv_filename')
 
         parser.add_argument(
             '--include_staff', '-s', action='store_true', default=False)
 
     def handle(self, *args, **options):
-        course = Course.objects.get(name=options['course_name'])
-        project = course.projects.get(name=options['project_name'])
+        project = Project.objects.get(pk=options['project_pk'])
+        course = project.course
 
         groups = project.submission_groups.all()
         submissions = [group.ultimate_submission for group in groups if group.submissions.count()]
 
-        row_headers = (['usernames', 'timestamp'] +
-                       [test.name for test in project.autograder_test_cases.all()] +
+        username_headers = ['username{}'.format(i + 1) for i in range(project.max_group_size)]
+        row_headers = (username_headers + ['timestamp'] +
+                       [test.name for test in
+                        project.autograder_test_cases.all().order_by('name')] +
                        ['total'])
         with open(options['csv_filename'], 'w') as f:
             writer = csv.DictWriter(f, fieldnames=row_headers)
@@ -35,8 +37,13 @@ class Command(BaseCommand):
             for submission in submissions:
                 if not options['include_staff'] and is_staff_submission(submission, course):
                     continue
-                row = {'usernames': '_'.join(submission.submission_group.member_names),
-                       'timestamp': submission.timestamp}
+                row = {'timestamp': submission.timestamp}
+                usernames_dict = dict(
+                    itertools.zip_longest(
+                        username_headers,
+                        sorted(submission.submission_group.member_names)))
+                row.update(usernames_dict)
+
                 total = 0
                 for result in submission.results.all():
                     fdbk = result.get_max_feedback()
