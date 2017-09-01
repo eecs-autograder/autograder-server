5e9e009c2f1a1dfd9fe45ca97f6cf7c9edae76e4
James P <jameslp@umich.edu>
2017-09-01 14:00:03 -0400

    Split dockerfiles into dev and production.

--- autograder/settings/production.py
+++ autograder/settings/production.py
@@ -7,3 +7,34 @@ DEBUG = False
 AG_TEST_MAX_RETRIES = 5
 AG_TEST_MIN_RETRY_DELAY = 7
 AG_TEST_MAX_RETRY_DELAY = 15
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'formatters': {
+        'verbose': {
+            'format': '[contactor] %(levelname)s %(asctime)s %(message)s'
+        },
+    },
+    'handlers': {
+        # Send all messages to console
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        # This is the "catch all" logger
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
+}
