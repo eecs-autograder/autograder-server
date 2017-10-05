93d7f7e98ab83e100c8cb3a0416879a05463ba44
ffuxa <ffuxa@umich.edu>
2017-10-05 16:37:44 -0400

    Started writing handgrading model tests

--- autograder/handgrading/models.py
+++ autograder/handgrading/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 from autograder.core.fields import EnumField
-import autograder.core.models as ag_models
 from django.core import validators
 from django.core.exceptions import ValidationError
 from enum import Enum
 
+from autograder.core.models import AutograderModel, Project, Submission
+
 
 class PointsStyle(Enum):
+    """
+    Ways hangrading points can be managed
+    """
     start_at_zero_and_add = "start_at_zero_and_add",
     start_at_max_and_subtract = "start_at_max_and_subtract"
 
 
-class HandgradingRubric(ag_models.AutogarderModel):
+class HandgradingRubric(AutograderModel):
     """
     The rubric which is linked to the project and adds or subtracts points from or to the total.
     """
@@ -25,34 +29,46 @@ class HandgradingRubric(ag_models.AutogarderModel):
 
     handgraders_can_apply_arbitrary_points = models.BooleanField()
 
-    project = models.ForeignKey(ag_models.Project)
+    project = models.ForeignKey(Project)
 
 
-class Criterion(ag_models.AutograderModel):
-    short_description = models.TextField()
+class Criterion(AutograderModel):
+    """
+    Rubric item with fixed points that is not line specific
+    """
+    short_description = models.TextField(blank=True)
 
-    long_description = models.TextField()
-
-    points = models.FloatField()
-
-    handgrading_rubric = models.ForeignKey(HandgradingRubric)
-
-
-class Annotation(ag_models.AutograderModel):
-    short_description = models.TextField()
-
-    long_description = models.TextField()
+    long_description = models.TextField(blank=True)
 
     points = models.FloatField()
 
     handgrading_rubric = models.ForeignKey(HandgradingRubric)
 
 
-class HandgradingResult(ag_models.AutograderModel):
-    submission = models.OneToOneField(ag_models.Submission)
+class Annotation(AutograderModel):
+    """
+    Additional field that can be applied to a submission. Can be line specific
+    """
+    short_description = models.TextField(blank=True)
+
+    long_description = models.TextField(blank=True)
+
+    points = models.FloatField()
+
+    handgrading_rubric = models.ForeignKey(HandgradingRubric)
 
 
-class CriterionResult(ag_models.AutograderModel):
+class HandgradingResult(AutograderModel):
+    """
+    Tied to a specific submission
+    """
+    submission = models.OneToOneField(Submission)
+
+
+class CriterionResult(AutograderModel):
+    """
+    Tied to a criterion object, specifies such criterion is selected
+    """
     selected = models.BooleanField()
 
     criterion = models.ForeignKey(Criterion)
@@ -60,51 +76,70 @@ class CriterionResult(ag_models.AutograderModel):
     handgrading_result = models.ForeignKey(HandgradingResult)
 
 
-class AppliedAnnotation(ag_models.AutograderModel):
-    comment = models.TextField(null=True,blank=True,default=None)
+class AppliedAnnotation(AutograderModel):
+    """
+    Tied to an annotation object, specifies where the annotation is applied and if a comment
+    was left with it
+    """
+    comment = models.TextField(blank=True)
 
-    location = models.OneToOneField(Location, related_name= 'location')
+    location = models.OneToOneField('Location', related_name='+')
 
     annotation = models.ForeignKey(Annotation)
 
     handgrading_result = models.ForeignKey(HandgradingResult)
 
     def clean(self):
-        if self.location.file_name not in self.handgrading_result.submission.submitted_filenames:
+        if self.location.filename not in self.handgrading_result.submission.submitted_filenames:
             raise ValidationError('Filename is not part of submitted files')
 
+    SERIALIZABLE_FIELDS = ('pk', 'last_modified', 'comment', 'annotation',
+                           'handgrading_result', 'location')
+    TRANSPARENT_TO_ONE_FIELDS = ('location',)
 
-class Comment(ag_models.AutograderModel):
-    location = models.OneToOneField(Location)
+
+class Comment(AutograderModel):
+    """
+    Comment left by staff or grader regarding submission. Can be applied to specific line
+    """
+    location = models.OneToOneField('Location')
 
     text = models.TextField()
 
     handgrading_result = models.ForeignKey(HandgradingResult)
 
+    def clean(self):
+        if self.location.filename not in self.handgrading_result.submission.submitted_filenames:
+            raise ValidationError('Filename is not part of submitted files')
 
-class ArbitraryPoints(ag_models.AutograderModel):
-    location = models.OneToOneField(Location, related_name='location')
 
-    text = models.TextField(null=True,blank=True,default=None)
+class ArbitraryPoints(AutograderModel):
+    """
+    Any arbitrary points specified by staff or grader for submission
+    """
+    location = models.OneToOneField('Location', related_name='+')
+
+    text = models.TextField(blank=True)
 
     points = models.FloatField()
 
     handgrading_result = models.ForeignKey(HandgradingResult)
 
     def clean(self):
-        if self.location.file_name not in self.handgrading_result.submission.submitted_filenames:
+        if self.location.filename not in self.handgrading_result.submission.submitted_filenames:
             raise ValidationError('Filename is not part of submitted files')
 
 
-class Location(ag_models.AutograderModel):
-    first_line = models.IntegerField()
+class Location(AutograderModel):
+    """
+    Defined as a block of code with a starting and ending line
+    """
+    first_line = models.IntegerField(validators=[validators.MinValueValidator(0)])
 
-    last_line = models.IntegerField()
+    last_line = models.IntegerField(validators=[validators.MinValueValidator(0)])
 
-    file_name = models.TextField(null=True,blank=True,default=None)
+    filename = models.TextField()
 
     def clean(self):
-        errors = {}
         if self.last_line < self.first_line:
-            raise  ValidationError('first line should be before last line')
-
+            raise ValidationError('first line should be before last line')
