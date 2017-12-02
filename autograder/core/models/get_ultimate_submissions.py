f8f5d70e9dd0661390e79f3bdc311da5886607fc
James P <jameslp@umich.edu>
2017-12-02 17:25:18 -0500

    Optimized get_ultimate_submissions.

--- autograder/core/models/get_ultimate_submissions.py
+++ autograder/core/models/get_ultimate_submissions.py
@@ -1,11 +1,12 @@
 import warnings
-from typing import Iterator
+from typing import Iterator, Iterable
 
 from django.db.models import Prefetch
 
 from .project import Project, UltimateSubmissionPolicy
 from .ag_test.feedback_category import FeedbackCategory
-from .submission import Submission
+from .submission_group import SubmissionGroup
+from .submission import Submission, get_submissions_with_results_queryset
 
 
 def get_ultimate_submission(project: Project, group_pk) -> Submission:
@@ -17,28 +18,39 @@ def get_ultimate_submission(project: Project, group_pk) -> Submission:
 
 
 def get_ultimate_submissions(project: Project, *group_pks) -> Iterator[Submission]:
-    groups = project.submission_groups.prefetch_related(
-        Prefetch(
-            'submissions',
-            queryset=Submission.objects.filter(
-                status=Submission.GradingStatus.finished_grading))).all()
+    finished_submissions_queryset = Submission.objects.filter(
+        status=Submission.GradingStatus.finished_grading)
+    base_group_queryset = project.submission_groups
+
     if group_pks:
-        groups = groups.filter(pk__in=group_pks)
+        base_group_queryset = base_group_queryset.filter(pk__in=group_pks)
 
     if project.ultimate_submission_policy == UltimateSubmissionPolicy.most_recent:
+        groups = base_group_queryset.prefetch_related(
+            Prefetch('submissions', finished_submissions_queryset))
         return (group.submissions.first() for group in groups if group.submissions.count())
     elif project.ultimate_submission_policy == UltimateSubmissionPolicy.best_with_normal_fdbk:
         warnings.warn('best_with_normal_fdbk is currently untested and may be deprecated soon.',
                       PendingDeprecationWarning)
-        groups = groups.prefetch_related(
-            'submissions__ag_test_suite_results__ag_test_case_results__ag_test_command_results')
-        return (
-            max(group.submissions.all(),
-                key=lambda submission: submission.get_fdbk(FeedbackCategory.normal).total_points)
-            for group in groups.all() if group.submissions.count())
+
+        submissions_queryset = get_submissions_with_results_queryset(
+            FeedbackCategory.normal, base_manager=finished_submissions_queryset)
+        groups = base_group_queryset.prefetch_related(
+            Prefetch('submissions', submissions_queryset))
+        return _best_submissions_generator(groups, FeedbackCategory.normal)
     elif project.ultimate_submission_policy == UltimateSubmissionPolicy.best:
-        groups = groups.prefetch_related(
-            'submissions__ag_test_suite_results__ag_test_case_results__ag_test_command_results')
-        return (max(group.submissions.all(),
-                    key=lambda submission: submission.get_fdbk(FeedbackCategory.max).total_points)
-                for group in groups.all() if group.submissions.count())
+        submissions_queryset = get_submissions_with_results_queryset(
+            FeedbackCategory.max, base_manager=finished_submissions_queryset)
+        groups = base_group_queryset.prefetch_related(Prefetch('submissions', submissions_queryset))
+        return _best_submissions_generator(groups, FeedbackCategory.max)
+
+
+def _best_submissions_generator(groups: Iterable[SubmissionGroup],
+                                fdbk_category: FeedbackCategory):
+    for group in groups:
+        submissions = list(group.submissions.all())
+        if len(submissions) == 0:
+            continue
+
+        yield max(submissions,
+                  key=lambda submission: submission.get_fdbk(fdbk_category).total_points)
