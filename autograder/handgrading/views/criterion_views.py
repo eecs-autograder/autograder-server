1caa3587a79760572df568046bb842eab2b98803
ffuxa <ffuxa@umich.edu>
2018-02-13 20:20:19 -0500

    Started criterion ordered view

--- autograder/handgrading/views/criterion_views.py
+++ autograder/handgrading/views/criterion_views.py
@@ -1,10 +1,11 @@
 import autograder.handgrading.models as handgrading_models
 import autograder.handgrading.serializers as handgrading_serializers
 import autograder.rest_api.permissions as ag_permissions
+from rest_framework import response
 
 from autograder.rest_api.views.ag_model_views import (
     AGModelGenericViewSet, ListCreateNestedModelView, TransactionRetrieveUpdateDestroyMixin,
-)
+    AGModelGenericView)
 
 
 class CriterionListCreateView(ListCreateNestedModelView):
@@ -23,8 +24,27 @@ class CriterionDetailViewSet(TransactionRetrieveUpdateDestroyMixin, AGModelGener
     serializer_class = handgrading_serializers.CriterionSerializer
     permission_classes = [
         ag_permissions.is_admin_or_read_only_staff(
-            lambda criterion: criterion.handgrading_rubric.project.course)
-    ]
+            lambda criterion: criterion.handgrading_rubric.project.course)]
+
     model_manager = handgrading_models.Criterion.objects.select_related(
-        'handgrading_rubric__project__course',
-    )
+        'handgrading_rubric__project__course',)
+
+
+class CriterionOrderView(AGModelGenericView):
+    permission_classes = [
+        ag_permissions.is_admin_or_read_only_staff(
+            lambda handgrading_rubric: handgrading_rubric.project.course)]
+
+    pk_key = 'handgrading_rubric_pk'
+    model_manager = handgrading_models.Criterion.objects.select_related(
+        'handgrading_rubric__project__course',)
+
+    def get(self, request, *args, **kwargs):
+        ag_test_suite = self.get_object()
+        return response.Response(list(ag_test_suite.get_agtestcase_order()))
+
+    def put(self, request, *args, **kwargs):
+        with transaction.atomic():
+            ag_test_suite = self.get_object()
+            ag_test_suite.set_agtestcase_order(request.data)
+            return response.Response(list(ag_test_suite.get_agtestcase_order()))
