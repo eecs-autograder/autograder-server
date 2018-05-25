e8bb543da3a03f3e71e337dc97cf47503c794fa1
James Perretta <jameslp@umich.edu>
2018-05-25 18:11:21 -0400

    Finished documentation for all ultimate submission results endpoint.

--- autograder/rest_api/views/submission_views/all_ultimate_submission_results_view.py
+++ autograder/rest_api/views/submission_views/all_ultimate_submission_results_view.py
@@ -1,22 +1,73 @@
+import copy
 import itertools
+from collections import OrderedDict
 
 from django.utils import timezone
 from drf_composable_permissions.p import P
+from drf_yasg import openapi
+from drf_yasg.openapi import Parameter, Schema
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import BasePermission
 
 from autograder.core.models.get_ultimate_submissions import get_ultimate_submissions
-from autograder.core.submission_feedback import SubmissionResultFeedback, AGTestPreLoader
+from autograder.core.submission_feedback import AGTestPreLoader
 from autograder.rest_api.views.ag_model_views import AGModelAPIView
 import autograder.rest_api.permissions as ag_permissions
 import autograder.core.models as ag_models
-from autograder.rest_api.views.schema_generation import APITags
+from autograder.rest_api.views.schema_generation import APITags, AGModelSchemaBuilder
 
 
 class _UltimateSubmissionsAvailable(BasePermission):
     def has_object_permission(self, request, view, project: ag_models.Project):
         closing_time_past = project.closing_time is None or project.closing_time < timezone.now()
         return not project.hide_ultimate_submission_fdbk and closing_time_past
+
+
+def _build_ultimate_submission_result_schema():
+    ultimate_submission_schema = Schema(
+        type='object',
+        properties=copy.deepcopy(
+            AGModelSchemaBuilder.get().get_schema(ag_models.Submission).properties)
+    )
+    assert (ultimate_submission_schema.properties is not
+            AGModelSchemaBuilder.get().get_schema(ag_models.Submission).properties)
+
+    ultimate_submission_schema.properties['results'] = Schema(
+        type='object',
+        properties=OrderedDict([
+            ('total_points', Schema(
+                type='float',
+            )),
+            ('total_points_possible', Schema(
+                type='float',
+            )),
+        ])
+    )
+
+    return Schema(
+        type='object',
+        properties=OrderedDict([
+            ('username', Schema(type='string')),
+            ('group', AGModelSchemaBuilder.get().get_schema(ag_models.Group)),
+            ('ultimate_submission', ultimate_submission_schema)
+        ])
+    )
+
+
+_all_ultimate_submission_results_schema = Schema(
+    type='object',
+    properties=OrderedDict([
+        ('count', openapi.Schema(type=openapi.TYPE_INTEGER)),
+        ('next', openapi.Schema(type=openapi.TYPE_STRING, format=openapi.FORMAT_URI)),
+        ('previous', openapi.Schema(type=openapi.TYPE_STRING, format=openapi.FORMAT_URI)),
+        ('results', Schema(
+            type='array',
+            items=_build_ultimate_submission_result_schema()
+
+        )),
+    ])
+)
 
 
 class UltimateSubmissionPaginator(PageNumberPagination):
@@ -34,6 +85,26 @@ class AllUltimateSubmissionResults(AGModelAPIView):
 
     api_tags = (APITags.submissions,)
 
+    @swagger_auto_schema(
+        manual_parameters=[
+            Parameter(name='page', type='integer', in_='query'),
+            Parameter(name='groups_per_page', type='integer', in_='query',
+                      default=UltimateSubmissionPaginator.page_size,
+                      description='Max groups per page: {}'.format(
+                          UltimateSubmissionPaginator.max_page_size)),
+            Parameter(
+                name='full_results', type='string', enum=['true', 'false'], in_='query',
+                description='When true, includes additional SubmissionResultFeedback details. '
+                            'Defaults to false.'
+            ),
+            Parameter(
+                name='include_staff', type='string', enum=['true', 'false'], in_='query',
+                description='When false, excludes staff and admin users '
+                            'from the results. Defaults to true.'
+            )
+        ],
+        responses={'200': _all_ultimate_submission_results_schema}
+    )
     def get(self, *args, **kwargs):
         project: ag_models.Project = self.get_object()
 
@@ -64,10 +135,7 @@ class AllUltimateSubmissionResults(AGModelAPIView):
                 submission_data = None
             else:
                 submission_data = submission.to_dict()
-                # submission_fdbk = SubmissionResultFeedback(
-                #     submission, ag_models.FeedbackCategory.max,
-                #     ag_test_preloader=ag_test_preloader
-                # )
+
                 if not full_results:
                     submission_results = {
                         'total_points': submission_fdbk.total_points,
