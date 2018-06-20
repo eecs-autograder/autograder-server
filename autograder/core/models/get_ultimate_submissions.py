2ab6e8764eda91b8b22fd37804d38085dcbe809b
James Perretta <jameslp@umich.edu>
2018-06-20 11:07:08 -0400

    Updated get_ultimate_submission to handle Submission.does_not_count_for

--- autograder/core/models/get_ultimate_submissions.py
+++ autograder/core/models/get_ultimate_submissions.py
@@ -1,6 +1,7 @@
 import warnings
-from typing import Iterator, Iterable, Optional
+from typing import Iterator, Optional, List
 
+from django.contrib.auth.models import User
 from django.db.models import Prefetch
 
 from autograder.core.submission_feedback import SubmissionResultFeedback, AGTestPreLoader
@@ -10,31 +11,34 @@ from .group import Group
 from .submission import Submission, get_submissions_with_results_queryset
 
 
-def get_ultimate_submission(group: Group) -> Optional[Submission]:
-    result = list(get_ultimate_submissions(group.project, group,
-                                           ag_test_preloader=AGTestPreLoader(group.project)))
-    if not result:
-        return None
-
-    return result[0].submission
+def get_ultimate_submission(group: Group, user: Optional[User]=None) -> Optional[Submission]:
+    project = group.project
+    [group] = _prefetch_submissions(project, group)
+    if project.ultimate_submission_policy == UltimateSubmissionPolicy.most_recent:
+        return _get_most_recent_submission(group, user)
+    elif project.ultimate_submission_policy == UltimateSubmissionPolicy.best_with_normal_fdbk:
+        best = _get_best_submission(
+            group,
+            FeedbackCategory.normal,
+            ag_test_preloader=AGTestPreLoader(project),
+            user=user
+        )
+        return best.submission if best is not None else None
+    elif project.ultimate_submission_policy == UltimateSubmissionPolicy.best:
+        best = _get_best_submission(
+            group,
+            FeedbackCategory.max,
+            ag_test_preloader=AGTestPreLoader(project),
+            user=user
+        )
+        return best.submission if best is not None else None
 
 
 def get_ultimate_submissions(
     project: Project, *groups: Group, ag_test_preloader: AGTestPreLoader
 ) -> Iterator[SubmissionResultFeedback]:
-
-    finished_submissions_queryset = Submission.objects.filter(
-        status=Submission.GradingStatus.finished_grading)
-    base_group_queryset = project.groups
-
-    if groups:
-        base_group_queryset = base_group_queryset.filter(pk__in=[group.pk for group in groups])
-
+    groups = _prefetch_submissions(project, *groups)
     if project.ultimate_submission_policy == UltimateSubmissionPolicy.most_recent:
-        submissions_queryset = get_submissions_with_results_queryset(
-            FeedbackCategory.max, base_manager=finished_submissions_queryset)
-        groups = base_group_queryset.prefetch_related(
-            Prefetch('submissions', submissions_queryset))
         return (SubmissionResultFeedback(group.submissions.first(),
                                          FeedbackCategory.max,
                                          ag_test_preloader)
@@ -43,36 +47,56 @@ def get_ultimate_submissions(
         warnings.warn('best_with_normal_fdbk is currently untested and may be deprecated soon.',
                       PendingDeprecationWarning)
 
-        submissions_queryset = get_submissions_with_results_queryset(
-            FeedbackCategory.normal, base_manager=finished_submissions_queryset)
-        groups = base_group_queryset.prefetch_related(
-            Prefetch('submissions', submissions_queryset))
-        # Workaround: We need to generate best submissions with normal feedback
+        # We need to generate best submissions with normal feedback
         # but return SubmissionResultFeedbacks with max feedback.
+        best_submissions_fdbks = (
+            _get_best_submission(group, FeedbackCategory.normal, ag_test_preloader)
+            for group in groups
+        )
+        best_submissions = (fdbk.submission for fdbk in best_submissions_fdbks
+                            if fdbk is not None)
         return (
-            SubmissionResultFeedback(fdbk.submission, FeedbackCategory.max, ag_test_preloader)
-            for fdbk in _best_submissions_generator(groups, FeedbackCategory.normal,
-                                                    ag_test_preloader))
+            SubmissionResultFeedback(submission, FeedbackCategory.max, ag_test_preloader)
+            for submission in best_submissions)
     elif project.ultimate_submission_policy == UltimateSubmissionPolicy.best:
-        submissions_queryset = get_submissions_with_results_queryset(
-            FeedbackCategory.max, base_manager=finished_submissions_queryset)
-        groups = base_group_queryset.prefetch_related(
-            Prefetch('submissions', submissions_queryset))
-        return _best_submissions_generator(groups, FeedbackCategory.max, ag_test_preloader)
+        best_submissions_fdbks = (
+            _get_best_submission(group, FeedbackCategory.max, ag_test_preloader)
+            for group in groups
+        )
+        return (fdbk for fdbk in best_submissions_fdbks if fdbk is not None)
 
 
-def _best_submissions_generator(groups: Iterable[Group],
-                                fdbk_category: FeedbackCategory,
-                                ag_test_preloader: AGTestPreLoader):
-    for group in groups:
-        submissions = list(group.submissions.all())
-        if len(submissions) == 0:
+def _prefetch_submissions(project: Project, *groups: Group) -> List[Group]:
+    finished_submissions_queryset = Submission.objects.filter(
+        status=Submission.GradingStatus.finished_grading)
+
+    base_group_queryset = project.groups
+    if groups:
+        base_group_queryset = base_group_queryset.filter(pk__in=[group.pk for group in groups])
+
+    submissions_queryset = get_submissions_with_results_queryset(
+        base_manager=finished_submissions_queryset)
+    return base_group_queryset.prefetch_related(Prefetch('submissions', submissions_queryset))
+
+
+def _get_most_recent_submission(group: Group, user: Optional[User]=None) -> Optional[Submission]:
+    for submission in group.submissions.all():
+        if user is None or user.username not in submission.does_not_count_for:
+            return submission
+
+    return None
+
+
+def _get_best_submission(group: Group, fdbk_category: FeedbackCategory,
+                         ag_test_preloader: AGTestPreLoader,
+                         user: Optional[User]=None) -> Optional[SubmissionResultFeedback]:
+    best: Optional[SubmissionResultFeedback] = None
+    for submission in group.submissions.all():
+        if user is not None and user.username in submission.does_not_count_for:
             continue
 
-        submission_results_fdbk = (
-            SubmissionResultFeedback(submission, fdbk_category, ag_test_preloader)
-            for submission in submissions
-        )
-        yield max(
-            submission_results_fdbk,
-            key=lambda submission_fdbk: submission_fdbk.total_points)
+        fdbk = SubmissionResultFeedback(submission, fdbk_category, ag_test_preloader)
+        if best is None or fdbk.total_points > best.total_points:
+            best = fdbk
+
+    return best
