cce59fff99b8c956748a0c7995ed47e77ecc03e1
James P <jameslp@umich.edu>
2018-11-17 23:56:30 -0500

    Version 3.0.7.

--- autograder/settings/base.py
+++ autograder/settings/base.py
@@ -8,7 +8,7 @@ import uuid
 
 from django.utils.crypto import get_random_string
 
-VERSION = '3.0.6'
+VERSION = '3.0.7'
 
 # This is the autograder-server directory
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
