7316fdf3366190722323bd63a40a3f7e0b33304d
James Perretta <jameslp@umich.edu>
2020-04-14 12:09:32 -0400

    Updated handgrading rubric views to use new base classes and schema gen.

--- autograder/handgrading/views/__init__.py
+++ autograder/handgrading/views/__init__.py
@@ -1,19 +1,9 @@
-from .annotation_views import (
-    AnnotationDetailViewSet, AnnotationListCreateView, AnnotationOrderView)
-
-from .applied_annotation_views import (
-    AppliedAnnotationDetailViewSet, AppliedAnnotationListCreateView)
-
-from .comment_views import (
-    CommentDetailViewSet, CommentListCreateView)
-
-from .criterion_result_views import (
-    CriterionResultDetailViewSet, CriterionResultListCreateView)
-
-from .criterion_views import (
-    CriterionDetailViewSet, CriterionListCreateView, CriterionOrderView)
-
+from .annotation_views import (AnnotationDetailViewSet, AnnotationListCreateView,
+                               AnnotationOrderView)
+from .applied_annotation_views import (AppliedAnnotationDetailViewSet,
+                                       AppliedAnnotationListCreateView)
+from .comment_views import CommentDetailViewSet, CommentListCreateView
+from .criterion_result_views import CriterionResultDetailViewSet, CriterionResultListCreateView
+from .criterion_views import CriterionDetailViewSet, CriterionListCreateView, CriterionOrderView
 from .handgrading_result_views import HandgradingResultView, ListHandgradingResultsView
-
-from .handgrading_rubric_views import (
-    HandgradingRubricDetailViewSet, HandgradingRubricRetrieveCreateViewSet)
+from .handgrading_rubric_views import GetCreateHandgradingRubricView, HandgradingRubricDetailView
