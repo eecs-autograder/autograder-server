ac54b2f8cf78fbb527f5ad0ffd6f336c1c1a804a
James Perretta <jameslp@umich.edu>
2020-07-09 10:11:05 -0400

    Added an 'all_bug_names' property to MutationTestSuiteResult.FeedbackCalculator and 'all_bug_names' field to BugsExposedFeedbackLevel. When this new feedback option is selected, the user will see a list of all buggy impl names in addition to a list of bugs exposed.

--- autograder/core/models/mutation_test_suite/mutation_test_suite.py
+++ autograder/core/models/mutation_test_suite/mutation_test_suite.py
@@ -15,6 +15,7 @@ class BugsExposedFeedbackLevel(core_ut.OrderedEnum):
     no_feedback = 'no_feedback'
     num_bugs_exposed = 'num_bugs_exposed'
     exposed_bug_names = 'exposed_bug_names'
+    all_bug_names = 'all_bug_names'
 
 
 class MutationTestSuiteFeedbackConfig(DictSerializableMixin):
