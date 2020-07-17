2812f08b1cfc347854e60bfbe1d9e22e27a0d76e
James Perretta <jameslp@umich.edu>
2020-07-17 09:45:55 -0400

    Added throttling to submission email decrypt endpoint.

--- autograder/core/urls.py
+++ autograder/core/urls.py
@@ -1,9 +1,19 @@
 from django.http import HttpResponse
 from django.urls import path
+from rest_framework.decorators import api_view, permission_classes, throttle_classes
+from rest_framework.response import Response
+from rest_framework.throttling import UserRateThrottle
 
 from autograder.core.submission_email_receipts import decrypt_message
 
 
+class DecryptEmailReceiptThrottle(UserRateThrottle):
+    rate = '60/minute'
+
+
+@api_view(['GET'])
+@permission_classes([])
+@throttle_classes([DecryptEmailReceiptThrottle])
 def decrypt_email_receipt_view(request, *args, encrypted_msg):
     return HttpResponse(
         decrypt_message(encrypted_msg), content_type="text/plain"
