b2a2dc13673fd15c299a901d612eddfa9332b264
James Perretta <jameslp@umich.edu>
2020-10-28 13:35:47 -0400

    Fixed an import alias.

--- autograder/utils/testing/__init__.py
+++ autograder/utils/testing/__init__.py
@@ -1,5 +1,5 @@
 from .misc import Timer as Timer
-from .misc import mocking_hook as mocking_hoo
+from .misc import mocking_hook as mocking_hook
 from .misc import timer as timer
 from .sleeper_subtest import sleeper_subtest
 from .unit_test_base import TransactionUnitTestBase as TransactionUnitTestBase
