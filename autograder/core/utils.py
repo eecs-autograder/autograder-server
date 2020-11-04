49694ef4779678e3f72ceb763f26cb134b3005ae
James Perretta <jameslp@umich.edu>
2020-11-04 16:20:14 -0500

    Added type annotations to autograder/core/utils.py

--- autograder/core/utils.py
+++ autograder/core/utils.py
@@ -5,9 +5,10 @@ import enum
 import os
 import re
 import subprocess
-from typing import List
 import typing
+from typing import List, Tuple, Type, TypeVar, cast
 
+import pytz
 from django.conf import settings
 from django.core import exceptions
 from django.utils import timezone
@@ -16,6 +17,9 @@ from . import constants as const
 
 if typing.TYPE_CHECKING:
     from .models.course import Course
+    from .models.project import Project
+    from .models.group import Group
+    from .models.submission import Submission
 
 
 class DiffResult:
@@ -28,10 +32,10 @@ _DIFF_LINE_REGEX = re.compile(r'^(?:  |\+ |- ).*\n+'.encode(), flags=re.MULTILIN
 
 
 def get_diff(first_filename: str, second_filename: str,
-             ignore_case=False,
-             ignore_whitespace=False,
-             ignore_whitespace_changes=False,
-             ignore_blank_lines=False) -> DiffResult:
+             ignore_case: bool = False,
+             ignore_whitespace: bool = False,
+             ignore_whitespace_changes: bool = False,
+             ignore_blank_lines: bool = False) -> DiffResult:
     """
     Diffs first and second using the GNU diff command line utility.
     Returns an empty list if first and second are considered equivalent.
@@ -64,8 +68,11 @@ def get_diff(first_filename: str, second_filename: str,
     return DiffResult(diff_result.returncode == 0, diff_list)
 
 
-def get_24_hour_period(start_time, contains_datetime: datetime.datetime,
-                       convert_result_to_utc=True):
+def get_24_hour_period(
+    start_time: datetime.time,
+    contains_datetime: datetime.datetime,
+    convert_result_to_utc: bool = True
+) -> Tuple[datetime.datetime, datetime.datetime]:
     """
     Returns a tuple (start_datetime, end_datetime) representing a 24
     hour period that contains the current date and time and with the
@@ -82,13 +89,12 @@ def get_24_hour_period(start_time, contains_datetime: datetime.datetime,
     end_datetime = start_datetime + timezone.timedelta(days=1)
 
     if convert_result_to_utc:
-        return (start_datetime.astimezone(timezone.pytz.UTC),
-                end_datetime.astimezone(timezone.pytz.UTC))
+        return start_datetime.astimezone(pytz.UTC), end_datetime.astimezone(pytz.UTC)
 
     return start_datetime, end_datetime
 
 
-def check_filename(filename):
+def check_filename(filename: str) -> None:
     """
     Verifies whether the given filename is valid according to the
     following requirements:
@@ -127,15 +133,14 @@ def get_course_relative_root_dir(course: Course) -> str:
     return os.path.join('courses', 'course{}'.format(course.pk))
 
 
-def get_project_root_dir(project):
+def get_project_root_dir(project: Project) -> str:
     """
     Computes the absolute path of the base directory for the given project.
     """
-    return os.path.join(
-        settings.MEDIA_ROOT, get_project_relative_root_dir(project))
+    return os.path.join(settings.MEDIA_ROOT, get_project_relative_root_dir(project))
 
 
-def get_project_relative_root_dir(project):
+def get_project_relative_root_dir(project: Project) -> str:
     """
     Same as get_project_root_dir() but returns a path that is
     relative to MEDIA_ROOT.
@@ -145,25 +150,23 @@ def get_project_relative_root_dir(project):
         'project{}'.format(project.pk))
 
 
-def get_project_files_dir(project):
+def get_project_files_dir(project: Project) -> str:
     """
     Computes the absolute path of the directory where uploaded files
     should be stored for the given project.
     """
-    return os.path.join(
-        settings.MEDIA_ROOT, get_project_files_relative_dir(project))
+    return os.path.join(settings.MEDIA_ROOT, get_project_files_relative_dir(project))
 
 
-def get_project_files_relative_dir(project):
+def get_project_files_relative_dir(project: Project) -> str:
     """
     Same as get_project_files_dir() but returns a path that is
     relative to MEDIA_ROOT.
     """
-    return os.path.join(
-        get_project_relative_root_dir(project), const.PROJECT_FILES_DIRNAME)
+    return os.path.join(get_project_relative_root_dir(project), const.PROJECT_FILES_DIRNAME)
 
 
-def get_project_groups_dir(project):
+def get_project_groups_dir(project: Project) -> str:
     """
     Computes the absolute path of the directory where student submission
     groups should be stored for the given project.
@@ -173,7 +176,7 @@ def get_project_groups_dir(project):
         get_project_groups_relative_dir(project))
 
 
-def get_project_groups_relative_dir(project):
+def get_project_groups_relative_dir(project: Project) -> str:
     """
     Same as get_project_groups_dir() but returns a path
     that is relative to MEDIA_ROOT.
@@ -183,27 +186,23 @@ def get_project_groups_relative_dir(project):
         const.PROJECT_SUBMISSIONS_DIRNAME)
 
 
-def get_student_group_dir(group):
+def get_student_group_dir(group: Group) -> str:
     """
     Computes the absolute path of the directory where submissions for the
     given group should be stored.
     """
-    return os.path.join(
-        settings.MEDIA_ROOT,
-        get_student_group_relative_dir(group))
+    return os.path.join(settings.MEDIA_ROOT, get_student_group_relative_dir(group))
 
 
-def get_student_group_relative_dir(group):
+def get_student_group_relative_dir(group: Group) -> str:
     """
     Same as get_student_group_dir() but returns a path that is
     relative to MEDIA_ROOT.
     """
-    return os.path.join(
-        get_project_groups_relative_dir(group.project),
-        'group{}'.format(group.pk))
+    return os.path.join(get_project_groups_relative_dir(group.project), 'group{}'.format(group.pk))
 
 
-def get_submission_dir(submission):
+def get_submission_dir(submission: Submission) -> str:
     """
     Computes the absolute path of the directory where files included
     in the given submission should be stored.
@@ -213,7 +212,7 @@ def get_submission_dir(submission):
         get_submission_relative_dir(submission))
 
 
-def get_submission_relative_dir(submission):
+def get_submission_relative_dir(submission: Submission) -> str:
     """
     Same as get_submission_dir() but returns a path that is relative to
     MEDIA_ROOT.
@@ -223,19 +222,21 @@ def get_submission_relative_dir(submission):
         get_submission_dir_basename(submission))
 
 
-def get_submission_dir_basename(submission):
+def get_submission_dir_basename(submission: Submission) -> str:
     return 'submission{}'.format(submission.pk)
 
 
-def get_result_output_dir(submission):
+def get_result_output_dir(submission: Submission)   -> str:
     return os.path.join(get_submission_dir(submission), 'output')
 
 
-def misc_cmd_output_dir():
+def misc_cmd_output_dir() -> str:
     return os.path.join(settings.MEDIA_ROOT, 'misc_cmd_output')
 
 
 # -----------------------------------------------------------------------------
+
+_OrderedEnumDerived = TypeVar('_OrderedEnumDerived', bound=enum.Enum)
 
 
 class OrderedEnum(enum.Enum):
@@ -253,44 +254,45 @@ class OrderedEnum(enum.Enum):
     >>> print(MyEnum.spam > MyEnum.egg)
     False
     """
+    _weight: int
 
     # Adopted from https://docs.python.org/3.5/library/enum.html#autonumber
-    def __new__(cls, value):
+    def __new__(cls: Type[_OrderedEnumDerived], value: object) -> _OrderedEnumDerived:
         obj = object.__new__(cls)
         obj._value_ = value
         # OrderedEnum values are ordered by _weight.
         obj._weight = len(cls.__members__)
-        return obj
+        return cast(_OrderedEnumDerived, obj)
 
     # Comparators adopted from https://docs.python.org/3.5/library/enum.html#orderedenum
-    def __ge__(self, other):
+    def __ge__(self, other: OrderedEnum) -> bool:
         if self.__class__ is other.__class__:
             return self._weight >= other._weight
 
         return NotImplemented
 
-    def __gt__(self, other):
+    def __gt__(self, other: OrderedEnum) -> bool:
         if self.__class__ is other.__class__:
             return self._weight > other._weight
 
         return NotImplemented
 
-    def __le__(self, other):
+    def __le__(self, other: OrderedEnum) -> bool:
         if self.__class__ is other.__class__:
             return self._weight <= other._weight
 
         return NotImplemented
 
-    def __lt__(self, other):
+    def __lt__(self, other: OrderedEnum) -> bool:
         if self.__class__ is other.__class__:
             return self._weight < other._weight
 
         return NotImplemented
 
     @classmethod
-    def get_min(cls):
+    def get_min(cls) -> _OrderedEnumDerived:
         return list(cls)[0]
 
     @classmethod
-    def get_max(cls):
+    def get_max(cls) -> _OrderedEnumDerived:
         return list(cls)[-1]
