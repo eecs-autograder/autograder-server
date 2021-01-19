bff940422cd8abc5974a9b7341e8b9c94c448b4b
James Perretta <jameslp@umich.edu>
2021-01-19 13:37:42 -0500

    Version 4.2.1

--- autograder/settings/base.py
+++ autograder/settings/base.py
@@ -7,7 +7,7 @@ import sys
 
 from django.utils.crypto import get_random_string
 
-VERSION = '4.2.0'
+VERSION = '4.2.1'
 
 # This is the autograder-server directory
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
